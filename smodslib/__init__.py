7e2d745620bb423d9f19b30c76619d8c30b2baf4
vincios <vinciosdev@gmail.com>
2022-07-20 12:47:02 +0200

    Added generate_download_url_from_id and search to the package exports

--- smodslib/__init__.py
+++ smodslib/__init__.py
@@ -1,3 +1,4 @@
 from .smods import create_mod_base_from_id as base_mod, create_full_mod_by_id as full_mod
-from .download import generate_download_url
+from .download import generate_download_url, generate_download_url_from_id
 from .batch import generate_dependency_tree
+from .catalogues import search
