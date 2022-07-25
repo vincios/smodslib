0cc49212e091fcef921422ab6a2645a88ad2ecab
Vincenzo <vinciosdev@gmail.com>
2022-07-25 15:49:26 +0200

    Added download_revision function

--- smodslib/download.py
+++ smodslib/download.py
@@ -1,9 +1,12 @@
+import os.path
+from typing import Callable, Union
 from urllib.parse import urlparse
 
 import cloudscraper
+from requests import HTTPError
 
 from .exceptions import UnsupportedHostError
-from .model import ModBase
+from .model import ModBase, ModRevision
 from .smods import create_mod_base_from_id
 
 SUPPORTED_HOSTS = ["modsbase.com", "uploadfiles.eu"]
@@ -58,3 +61,58 @@ def generate_download_url(mod: ModBase):
         download_url = r.headers['Location']
 
     return download_url
+
+
+def download_revision(download_url: str, download_path: str, progress_callback: Callable[[int, int], None] = None,
+                      error_callback: Callable[[int, str], None] = None) -> Union[str, None]:
+    """
+    Tries to download a mod revision to a given download path. It tries to bypass cloudflare protection, but in case of
+    failure, an error 403 will be raised from the error_callback. The progress_callback instead will be called when a
+    new chunk of bytes has been downloaded successfully.
+    download bytes.
+
+    If file already exists the function will return immediately.
+
+    :param download_url: ModRevision download url generated with generate_download_url function
+    :param download_path: path where the zipped file will be downloaded
+    :param progress_callback: callback of type (downloaded_bytes: int, total_bytes: int) -> None
+    :param error_callback: callback of type (http_error_code, error_content)
+    :return: The downloaded file path
+    """
+    url_parts = download_url.split('/')
+    local_filename = url_parts[-1]
+
+    download_target = os.path.join(os.path.abspath(download_path), local_filename)
+    if os.path.exists(download_target):
+        if progress_callback:
+            size = os.path.getsize(download_target)
+            progress_callback(size, size)
+
+        return os.path.abspath(download_target)
+
+    scraper = cloudscraper.create_scraper()
+
+    # NOTE the stream=True parameter below
+    with scraper.get(download_url, stream=True) as r:
+        try:
+            r.raise_for_status()
+        except HTTPError as e:
+            if error_callback:
+                error_callback(r.status_code, str(e))
+
+            return None
+
+        total_length = r.headers.get('content-length')
+
+        with open(download_target, 'wb') as f:
+            dl = 0
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                dl += len(chunk)
+                f.write(chunk)
+
+                if total_length and progress_callback:
+                    progress_callback(dl, int(total_length))
+
+    return os.path.abspath(download_target)
